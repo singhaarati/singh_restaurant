def93e197fe85f43ab1c91a08ff634f8ea1b97bb
aarati0307 <aarati.singh@patancollege.edu.np>
2022-07-14 13:30:20 +0545

    buy database

--- home/views.py
+++ home/views.py
@@ -5,4 +5,16 @@ def index(request):
     return render(request,'home/index.html')
 
 def buy_page(request):
-    return render(request,'home/buy.html')
+    
+        if  request.method == "POST":
+            firstname= request.POST.get('firstname')
+            lastname= request.POST.get('lastname')
+            username= request.POST.get('username')
+            email= request.POST.get('email')
+            mobile= request.POST.get('mobile')
+            address= request.POST.get('address')
+            zipcode= request.POST.get('zipcode')
+            buy = buy(firstname=firstname, lastname=lastname, username=username, email=email, mobile=mobile, address=address, zipcode=zipcode)
+            buy.save()
+            
+        return render(request, 'home/buy.html')  
